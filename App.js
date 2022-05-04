1e0d94dcd46b56c28eea412badc96f87e0e13d3d
Sandile-Repos <sandile.developer@gmail.com>
2022-05-04 18:16:42 +0200

    add startGameScreen and,  its input and custom button component

--- App.js
+++ App.js
@@ -1,77 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, View, FlatList, Button } from "react-native";
-import GoalInput from "./src/components/GoalInput";
-import GoalItem from "./src/components/GoalItem";
+import { StyleSheet } from "react-native";
+
+import StartGameScreen from "./src/screen/StartGameScreen";
 
 export default function App() {
-  const [modalIsVisible, setModalIsVisble] = useState(false);
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  function startAddGoalHandler() {
-    setModalIsVisble(true);
-  }
-  function endAddGoalHandler() {
-    setModalIsVisble(false);
-  }
-
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ]);
-    // setModalIsVisble(false);
-    endAddGoalHandler();
-  }
-
-  function deleteGoalHandler(id) {
-    setCourseGoals((currentCourseGoals) => {
-      return currentCourseGoals.filter((goal) => goal.id !== id);
-    });
-  }
-
-  return (
-    <>
-      <StatusBar style="light" />
-      <View style={styles.appContainer}>
-        <Button
-          title="Add New Goal"
-          color="#a065ec"
-          onPress={startAddGoalHandler}
-        />
-        <GoalInput
-          onAddGoal={addGoalHandler}
-          visible={modalIsVisible}
-          onCancel={endAddGoalHandler}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            keyExtractor={(item, index) => item.id}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  text={itemData.item.text}
-                  id={itemData.item.id}
-                  onDeleteItem={deleteGoalHandler}
-                />
-              );
-            }}
-          />
-        </View>
-      </View>
-    </>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-    // backgroundColor:'#1e085a' //can have on app.json to configure for all main screens as backgroundcolor
-  },
-  goalsContainer: {
-    flex: 5,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ src/components/PrimaryButton.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, Text, View, Pressable } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  const pressHandler = () => {
+    console.warn("Pressed");
+  };
+  return (
+    <View style={styles.buttonOutterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.button}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOutterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  button: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
