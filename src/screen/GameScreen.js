af296e31935a81e2503514c96c36228ef7c80cc0
Sandile-Repos <sandile.developer@gmail.com>
2022-05-04 19:52:41 +0200

    add safeAreaView

--- src/screen/GameScreen.js
+++ src/screen/GameScreen.js
@@ -3,12 +3,25 @@ import { StyleSheet, Text, View } from "react-native";
 
 const GameScreen = () => {
   return (
-    <View>
-      <Text>Game Screen</Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* Guess */}
+      <View>
+        <Text>Higer or lower</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
     </View>
   );
 };
 
 export default GameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  containers: {},
+  container: {},
+});
