2cfe9217dfe7257b20260017b71d6162ec053f1c
Sandile-Repos <sandile.developer@gmail.com>
2022-05-06 22:37:07 +0200

    display meals and add images and styling

--- src/screens/MealsOverviewScreen.js
+++ src/screens/MealsOverviewScreen.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { useRoute } from "@react-navigation/native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import MealItem from "../components/MealItem";
+// import { useRoute } from "@react-navigation/native";
 
 import { MEALS } from "../data/dummy-data";
-
 const MealsOverviewScreen = ({ route }) => {
   //   const route = useRoute();
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen -{catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
