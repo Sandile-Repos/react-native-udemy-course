73f1bb278374fa035dfeb2b8c598aba2585f374b
Sandile-Repos <sandile.developer@gmail.com>
2022-05-14 19:10:10 +0200

    bottom tap

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
+// import "react-native-gesture-handler";
+// import { View, StyleSheet, Text, Button, StatusBar } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+// import { Ionicons } from "@expo/vector-icons";
+
+// const Drawer = createDrawerNavigator();
+
+// import WelcomeScreen from "./src/screens/WelcomeScreen";
+// import UserScreen from "./src/screens/UserScreen";
+
+// export default function App() {
+//   return (
+//     <>
+//       <StatusBar style="light" />
+//       <NavigationContainer>
+//         <Drawer.Navigator
+//           screenOptions={{
+//             headerStyle: { backgroundColor: "#3c0a6b" },
+//             headerTintColor: "white",
+//             drawerActiveBackgroundColor: "#f0e1ff",
+//             drawerActiveTintColor: "#3c0a6b",
+//             // drawerInactiveTintColor: "green",
+//             // drawerStyle: { backgroundColor: "#ccc" },
+//           }}
+//         >
+//           <Drawer.Screen
+//             name="Welcome"
+//             component={WelcomeScreen}
+//             options={{
+//               drawerLabel: "Welcome Screen",
+//               drawerIcon: ({ color, size }) => (
+//                 <Ionicons size={size} name="home" color={color} />
+//               ),
+//             }}
+//           />
+//           <Drawer.Screen
+//             name="User"
+//             component={UserScreen}
+//             options={{
+//               drawerIcon: ({ color, size }) => (
+//                 <Ionicons size={size} name="person" color={color} />
+//               ),
+//             }}
+//           />
+//         </Drawer.Navigator>
+//       </NavigationContainer>
+//     </>
+//   );
+// }
 import "react-native-gesture-handler";
 import { View, StyleSheet, Text, Button, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 import WelcomeScreen from "./src/screens/WelcomeScreen";
 import UserScreen from "./src/screens/UserScreen";
@@ -14,36 +64,32 @@ export default function App() {
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <Drawer.Navigator
+        <BottomTab.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: "#3c0a6b" },
             headerTintColor: "white",
-            drawerActiveBackgroundColor: "#f0e1ff",
-            drawerActiveTintColor: "#3c0a6b",
-            // drawerInactiveTintColor: "green",
-            // drawerStyle: { backgroundColor: "#ccc" },
+            tabBarActiveTintColor: "#3c0a6b",
           }}
         >
-          <Drawer.Screen
+          <BottomTab.Screen
             name="Welcome"
             component={WelcomeScreen}
             options={{
-              drawerLabel: "Welcome Screen",
-              drawerIcon: ({ color, size }) => (
-                <Ionicons size={size} name="home" color={color} />
+              tabBarIcon: ({ size, color }) => (
+                <Ionicons name="home" color={color} size={size} />
               ),
             }}
           />
-          <Drawer.Screen
+          <BottomTab.Screen
             name="User"
             component={UserScreen}
             options={{
-              drawerIcon: ({ color, size }) => (
-                <Ionicons size={size} name="person" color={color} />
+              tabBarIcon: ({ size, color }) => (
+                <Ionicons name="person" color={color} size={size} />
               ),
             }}
           />
-        </Drawer.Navigator>
+        </BottomTab.Navigator>
       </NavigationContainer>
     </>
   );
