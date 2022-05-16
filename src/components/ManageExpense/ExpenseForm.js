015c74aea92a61936b9c5582421ba948e6a2bd8e
Sandile-Repos <sandile.developer@gmail.com>
2022-05-16 23:08:32 +0200

    adding styling

--- src/components/ManageExpense/ExpenseForm.js
+++ src/components/ManageExpense/ExpenseForm.js
@@ -1,31 +1,34 @@
 import React from "react";
 import { StyleSheet, TextInput, View } from "react-native";
-import Input from "./input";
+import Input from "./Input";
 
 const ExpenseForm = () => {
-    const amountChangeHandler = () =>{}
+  const amountChangeHandler = () => {};
   return (
     <View>
-      <Input 
-      label="Amount" 
-      textInputConfig={{
-          keyboardType='decimal-pad',
-          onChangeText:amountChangeHandler,
-      }}
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangeHandler,
+        }}
       />
-      <Input 
-      label="Date" 
-      textInputConfig={{
-          placeHolder:'YYYY-MM-DD', 
-          maxLength:10, 
-          onChangeText:()=>{}
-        }} />
-      <Input 
-      label="Description" textInputConfig={{
-          multiline:true,
-        //   autoCorrect:false //default is true
-        //   autoCapitalize:'none' //default is 'sentences'
-        }} />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          //   autoCorrect:false //default is true
+          //   autoCapitalize:'none' //default is 'sentences'
+        }}
+      />
     </View>
   );
 };
--- src/components/ManageExpense/Input.js
+++ src/components/ManageExpense/Input.js
@@ -1,17 +1,45 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
+import { GlobalStyles } from "../constants/styles";
 
 // const Input = ({label, type, maxLength}) => {
 const Input = ({ label, textInputConfig }) => {
+  let inputStyles = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
       {/* <TextInput keyboardType={type} maxLength={maxLength} /> */}
-      <TextInput {...textInputConfig} />
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
