7cf6ab24d6fedf2116210b71db86afca8af6558e
Sandile-Repos <sandile.developer@gmail.com>
2022-05-16 17:24:47 +0200

    add delete button

--- src/screens/ManageExpense.js
+++ src/screens/ManageExpense.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { borderTopColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import { GlobalStyles } from "../components/constants/styles";
+import IconButton from "../components/UI/IconButton";
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -11,13 +14,37 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
+  const deleExpenseHandler = () => {};
+
   return (
-    <View>
-      <Text>ManageExpense Screen</Text>
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleExpenseHandler}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 export default ManageExpense;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
