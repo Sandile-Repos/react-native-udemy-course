27b10081dcd74f23e713ba12c47087250191b7d1
Sandile-Repos <sandile.developer@gmail.com>
2022-05-20 11:47:58 +0200

    add custom button

--- src/components/Places/ImagePicker.js
+++ src/components/Places/ImagePicker.js
@@ -1,11 +1,16 @@
-import React from "react";
-import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Alert, Image } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from "expo-image-picker";
+import { Colors } from "../../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
+
 const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState();
+
   const [CameraPermissionsInformation, requestPermission] =
     useCameraPermissions();
 
@@ -38,16 +43,38 @@ const ImagePicker = () => {
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    setPickedImage(image.uri);
   };
+
+  let imagePreview = <Text>No image taken yet..</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 };
 
 export default ImagePicker;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
