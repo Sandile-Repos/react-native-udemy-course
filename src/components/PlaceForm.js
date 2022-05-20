ccda4e97121412129d36b520f8e81c404c2f090e
Sandile-Repos <sandile.developer@gmail.com>
2022-05-20 08:47:44 +0200

    add some custom form

--- src/components/PlaceForm.js
+++ src/components/PlaceForm.js
@@ -1,14 +1,36 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, ScrollView } from "react-native";
+import { Colors } from "../../constants/colors";
 
 const PlaceForm = () => {
+  const [enteredTile, setEnteredTile] = useState("");
+  const changeTitleHandler = (enteredText) => {
+    setEnteredTile(enteredText);
+  };
   return (
-    <View>
-      <Text>The Place Form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <Text style={styles.label}>Title</Text>
+      <TextInput
+        style={styles.input}
+        value={enteredTile}
+        onChangeText={changeTitleHandler}
+      />
+    </ScrollView>
   );
 };
 
 export default PlaceForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: { flex: 1, padding: 24 },
+  label: { fontWeight: "bold", marginBottom: 4, color: Colors.primary500 },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
